f38570dd221db381ea11ef6bbb84f1b7bc2ade2d
szajbka <sfiluto@gmail.com>
2022-12-02 11:51:24 +0100

    improvement

--- 002/score_pt1.py
+++ 002/score_pt1.py
@@ -2,26 +2,15 @@ with open("002\input.txt") as file:
     total = 0
     line = file.readline()
     while line:
-        if line[0] == 'A':
-            if line[2] == 'X':
-                total+=4
-            elif line[2] == 'Y':
-                total+=8
-            elif line[2] == 'Z':
-                total+=3
-        elif line[0] == 'B':
-            if line[2] == 'X':
-                total+=1
-            elif line[2] == 'Y':
-                total+=5
-            elif line[2] == 'Z':
-                total+=9
-        elif line[0] == 'C':
-            if line[2] == 'X':
-                total+=7
-            elif line[2] == 'Y':
-                total+=2
-            elif line[2] == 'Z':
-                total+=6
+        a,b = line.split()
+        a="ABC".index(a)
+        b="XYZ".index(b)
+        total+=(b+1)
+
+        if(a-b)==0:
+            total+=3
+        elif(a-b)==-1 or (a-b)==2:
+            total+=6
+                
         line = file.readline()
     print(str(total))
